c896a9c068915ba1011c38ff5258c028b9d81316
Octavian Biris <octavian.biris@visionsystemsinc.com>
2016-07-07 17:44:05 -0400

    Updated Makefile with emacs packages

--- Makefile
+++ Makefile
@@ -1,8 +1,7 @@
+all: emacs pymacs
+	git submodule update
 pymacs:
 	cd ./lisp/plugins/Pymacs
 	make install
-all:
-	emacs -e "(progn (package-initialize)(package-install 'auctex))"
-	emacs -e "(progn (package-initialize)(package-install 'jedi))"
-	emacs -e "(progn (package-initialize)(package-install 'magit))"
-	emacs -e "(progn (package-initialize)(package-install 'zenburn-theme))"
+emacs:
+	emacs  --batch --load packages.el --eval "(install-packages)"
---
+++ packages.el
@@ -0,0 +1,17 @@
+(defvar toinstall '(zenburn-theme company company-math jedi auctex auto-complete popup autopair flymake-cursor bash-completion))
+(defun install-packages()
+  (progn
+    (package-initialize)
+    (require 'package)
+    (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives     '("marmalade" . "http://marmalade-repo.org/packages/"))
+    (package-refresh-contents)
+    (dolist (p toinstall) (safe-install-package p))
+    ))
+
+(defun safe-install-package(p)
+    (when (not (package-installed-p p))
+      (message "%s %s %s" "====== Installing Package :" p "=======")
+      (package-install p)
+      )
+)
